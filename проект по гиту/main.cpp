230e42c0448eb35da259828934feeea4ac1d3cc4
Darina Gneusneva <krokodilka2006@gmail.com>
2024-09-24 11:12:54 +0300

    математическая операция

--- проект по гиту/main.cpp
+++ проект по гиту/main.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(void) {
+    int a;
+    int b;
+    a=11;
+    b = 10;
+    printf("%d\n", a+b);
 }
